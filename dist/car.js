6b7f6aa92e0d6cc971d356f543ce42d8267ddaf6
Дмитрий Крюков <demob@MacBook-Pro-Dmitrij.local>
2024-10-24 16:41:39 +0400

    Add CarSystem super with a general logic

--- dist/car.js
+++ dist/car.js
@@ -1,59 +1,62 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Car = void 0;
-class BasicEngine {
-    constructor() {
-        this.running = false;
+class CarSystem {
+    constructor(car) {
+        this.isActive = false;
+        this.car = car;
+    }
+    getStatus(activeMessage, inactiveMessage) {
+        return this.isActive ? activeMessage : inactiveMessage;
+    }
+}
+class BasicEngine extends CarSystem {
+    constructor(car) {
+        super(car);
     }
     start() {
-        this.running = true;
+        this.isActive = true;
         console.log("%c▶️ Двигатель запущен.", "color: green;");
     }
     stop() {
-        this.running = false;
+        this.isActive = false;
         console.log("%c⏸ Двигатель заглушен.", "color: red;");
     }
     getStatus() {
-        return this.running ? "Двигатель работает." : "Двигатель не работает.";
+        return super.getStatus("Двигатель активен.", "Двигатель не активен.");
     }
 }
-class BasicWheels {
-    constructor() {
-        this.rotating = false;
-    }
+class BasicWheels extends CarSystem {
     rotate() {
-        this.rotating = true;
+        this.isActive = true;
         console.log("%c▶️ Колёса начали вращаться.", "color: green;");
     }
     brake() {
-        this.rotating = false;
+        this.isActive = false;
         console.log("%c⏸ Колёса заблокированы.", "color: red;");
     }
     getStatus() {
-        return this.rotating ? "Колёса вращаются." : "Колёса не вращаются.";
+        return super.getStatus("Колёса вращаются.", "Колёса не вращаются.");
     }
 }
-class BasicLights {
-    constructor() {
-        this.on = false;
-    }
+class BasicLights extends CarSystem {
     turnOn() {
-        this.on = true;
+        this.isActive = true;
         console.log("%c▶️ Фары включены.", "color: green;");
     }
     turnOff() {
-        this.on = false;
+        this.isActive = false;
         console.log("%c⏸ Фары выключены.", "color: red;");
     }
     getStatus() {
-        return this.on ? "Фары горят." : "Фары не горят.";
+        return super.getStatus("Фары горят.", "Фары не горят.");
     }
 }
 class Car {
     constructor() {
-        this.engine = new BasicEngine();
-        this.wheels = new BasicWheels();
-        this.lights = new BasicLights();
+        this.engine = new BasicEngine(this);
+        this.wheels = new BasicWheels(this);
+        this.lights = new BasicLights(this);
     }
     displayInfo() {
         const statuses = [
--- src/car.ts
+++ src/car.ts
@@ -1,3 +1,6 @@
+interface ICarSystem {
+    getStatus(activeMessage: string, inactiveMessage: string): string;
+}
 interface IEngine {
     start(): void;
     stop(): void;
@@ -23,56 +26,69 @@ interface ICar {
     displayInfo(): void;
 }
 
-class BasicEngine implements IEngine {
-    private running: boolean = false;
+class CarSystem implements ICarSystem {
+    protected car: ICar;
+    protected isActive: boolean = false;
 
-    start(): void  {
-        this.running = true;
+    constructor(car: ICar) {
+        this.car = car;
+    }
+
+    getStatus(activeMessage: string, inactiveMessage: string): string {
+        return this.isActive ? activeMessage : inactiveMessage;
+    }
+}
+
+
+class BasicEngine extends CarSystem implements IEngine {
+    constructor(car: ICar) {
+        super(car);
+    }
+
+    start(): void {
+        this.isActive = true;
         console.log("%c▶️ Двигатель запущен.", "color: green;");
     }
 
     stop(): void {
-        this.running = false;
+        this.isActive = false;
         console.log("%c⏸ Двигатель заглушен.", "color: red;");
     }
 
     getStatus(): string {
-        return this.running ? "Двигатель работает." : "Двигатель не работает.";
+        return super.getStatus("Двигатель активен.", "Двигатель не активен.");
     }
 }
 
-class BasicWheels implements IWheels {
-    private rotating: boolean = false;
-
-    rotate(): void  {
-        this.rotating = true;
+class BasicWheels extends CarSystem implements IWheels {
+    rotate(): void {
+        this.isActive = true;
         console.log("%c▶️ Колёса начали вращаться.", "color: green;");
     }
-    brake(): void  {
-        this.rotating = false;
+
+    brake(): void {
+        this.isActive = false;
         console.log("%c⏸ Колёса заблокированы.", "color: red;");
     }
 
     getStatus(): string {
-        return this.rotating ? "Колёса вращаются." : "Колёса не вращаются.";
+        return super.getStatus("Колёса вращаются.", "Колёса не вращаются.");
     }
 }
 
-class BasicLights implements ILights {
-    private on: boolean = false;
-
+class BasicLights extends CarSystem implements ILights {
     turnOn(): void {
-        this.on = true;
+        this.isActive = true;
         console.log("%c▶️ Фары включены.", "color: green;");
     }
 
     turnOff(): void {
-        this.on = false;
+        this.isActive = false;
         console.log("%c⏸ Фары выключены.", "color: red;");
     }
 
     getStatus(): string {
-        return this.on ? "Фары горят." : "Фары не горят.";
+        return super.getStatus("Фары горят.", "Фары не горят.");
     }
 }
 
@@ -82,9 +98,9 @@ class Car implements ICar {
     lights: ILights;
 
     constructor() {
-        this.engine = new BasicEngine();
-        this.wheels = new BasicWheels();
-        this.lights = new BasicLights();
+        this.engine = new BasicEngine(this);
+        this.wheels = new BasicWheels(this);
+        this.lights = new BasicLights(this);
     }
 
     displayInfo() {
